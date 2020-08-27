90d13854a59561552f9b45a94282f2b4df797b0c
varnikagautam <54426822+varnikagautam@users.noreply.github.com>
2020-08-28 00:13:44 +0530

    Update ExpectedReport.cs

--- ExpectedReport.cs
+++ ExpectedReport.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Diagnostics;
 
+namespace VitalsSimplification
+{
+   class ExpectedReports
+    {
+      public static void ExpectTrue(bool expression) 
+        {
+          if(!expression) 
+            {
+              Console.WriteLine("Expected true, but got false");
+              Environment.Exit(1);
+            }
+        }
+   
+      public static void ExpectFalse(bool expression) 
+          {
+             if(expression) 
+              {
+                Console.WriteLine("Expected false, but got true");
+                Environment.Exit(1);
+              }
+          }
+          
+     }
+}
